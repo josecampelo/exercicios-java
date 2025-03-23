0ed9a908e1e77b7628c7470b64e971fcce084fa5
Campelo <josecampelojr@gmail.com>
2025-03-23 17:17:43 -0300

    Resolução do exercício 03 sobre Array

---
+++ src/exercicios/arrays/Exercicio03.java
@@ -0,0 +1,22 @@
+package exercicios.arrays;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        int[] numeros = {7, 14, 26, 37, 2, 80, 36, 41};
+        int menorNumero = numeros[0];
+        int maiorNumero = numeros[0];
+
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < menorNumero) {
+                menorNumero = numeros[i];
+            }
+
+            if (numeros[i] > maiorNumero) {
+                maiorNumero = numeros[i];
+            }
+        }
+
+        System.out.println("Menor número do Array: " + menorNumero);
+        System.out.println("Maior número do Array: " + maiorNumero);
+    }
+}
